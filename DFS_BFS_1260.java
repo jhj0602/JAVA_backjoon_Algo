44d84bb189fb77c3b9c55e10b56004c9b78a7a30
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-08-25 17:03:22 +0900

    Update DFS_BFS_1260.java

--- DFS_BFS_1260.java
+++ DFS_BFS_1260.java
@@ -1,16 +1,18 @@
-package backjoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.List;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class DFS_BFS_1260 {
+public class DFSBFS_1260 {
 	static int[][] map;
 	static boolean[] visit;
 	static int n, m, v;
+	static List<Integer>[] node;
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,54 +20,58 @@ public class DFS_BFS_1260 {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		v = Integer.parseInt(st.nextToken());
-		map = new int[n + 1][n + 1];
 		visit = new boolean[n + 1];
-		int num1, num2;
+		node = new List[n + 1];
+		for (int i = 0; i < n + 1; i++) {
+			node[i] = new ArrayList<>();
+		}
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			num1 = Integer.parseInt(st.nextToken());
-			num2 = Integer.parseInt(st.nextToken());
-			map[num1][num2] = map[num2][num1] = 1;
-
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			node[x].add(y);
+			node[y].add(x);
 		}
+		for (int i = 0; i < n; i++) {
+			Collections.sort(node[i]);
+		}
+		
+		visit[v] = true;
+		System.out.print(v + " ");
 		dfs(v);
-		ResetVisit();
-		bfs(v);
-
-	}
-
-	public static void ResetVisit() {
-		for (int i = 1; i < n + 1; i++) {
-			visit[i] = false;
-		}
+		visit = new boolean[n + 1];
 		System.out.println();
+		bfs();
+
 	}
 
-	public static void dfs(int d) {
-		visit[d] = true;
-		System.out.print(d + " ");
-		for (int i = 1; i < n + 1; i++) {
-			if (map[d][i] == 1 && !visit[i]) {
-				dfs(i);
+	public static void dfs(int v) {
+		for (int i = 0; i < node[v].size(); i++) {
+			int childNode = node[v].get(i);
+			if (!visit[childNode]) {
+				visit[childNode] = true;
+				System.out.print(childNode + " ");
+				dfs(childNode);
 			}
 		}
 	}
 
-	public static void bfs(int b) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.add(b);// 맨끝에다가 데이터를 집어넣어줌 peek() 맨앞에 데이터를 보는것
-		visit[b] = true;
-		while (!queue.isEmpty()) {// isEmpty 비엇는지 확인하는것
-			b = queue.poll(); // 가장먼저 보관한 값 꺼내고 반환
-			System.out.print(b + " ");
-			for (int i = 1; i < n + 1; i++) {
-				if (map[b][i] == 1 && !visit[i]) {
-					queue.offer(i);//자료를 보관하는 큐 메소드
-					visit[i] = true;
-				}
-			}
-		}
-	
+	public static void bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        visit[v]=true;
+        System.out.print(v+" ");
+        while(!queue.isEmpty()) {
+        	int temp = queue.poll();
+        	for(int i=0;i<node[temp].size();i++) {
+        		int childNode = node[temp].get(i);
+        		if(!visit[childNode]) {
+        			queue.offer(childNode);
+        			System.out.print(childNode+" ");
+        			visit[childNode]=true;
+        		}
+        	}
+        }
 	}
 
 }
