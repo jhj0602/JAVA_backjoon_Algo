9c0ed746be18200b5d69befe809bed82d4db6d31
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-11-09 03:21:52 +0900

    BFS

--- BFS_7576.java
+++ BFS_7576.java
@@ -7,56 +7,79 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+class dot {
+	int x;
+	int y;
+
+	dot(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 public class BFS_7576 {
-	static int M, N;
+
 	static int[] dx = { 0, -1, 0, 1 };
 	static int[] dy = { 1, 0, -1, 0 };
-	static int[][] arr;
-	static boolean[][] visit;
-	static int count;
+	static int [][] arr;
+	static int M,N;
+	static Queue<dot> q = new LinkedList<dot>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
 		arr = new int[N][M];
-		visit = new boolean[1000][1000];
+
 		for (int i = 0; i < N; i++) {
 			String[] line = br.readLine().split(" ");
 			for (int j = 0; j < M; j++) {
 				arr[i][j] = Integer.parseInt(line[j]);
-				if (arr[i][j] == 1 || arr[i][j] == -1) {
-					visit[i][j] = true;
+				if (arr[i][j] == 1) {
+					q.add(new dot(i, j));
 				}
 
 			}
-		}
 
+		}
+		bfs();
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j] == 0) {
+					
+					System.out.println(-1);
+					return;
+				}
+				max = Math.max(max, arr[i][j]);
+			}
+		}
+	
+		System.out.println(max - 1);
 	}
 
-	static void bfs(int x, int y) {
-		Queue<int[]> queue = new LinkedList<int[]>();
-
-		queue.add(new int[] { x, y });
-		while (!queue.isEmpty()) {
-			x = queue.peek()[0];
-			y = queue.peek()[1];
-			queue.poll();
-			visit[x][y] = true;
+	public static void bfs( ) {
+		
+		
+		while (!q.isEmpty()) {
+			
+			dot dot = q.poll();
 			for (int i = 0; i < 4; i++) {
-				int xq = x + dx[i];
-				int yq = y + dy[i];
-				if (xq >= 0 & yq >= 0 & xq < M & yq < N) {
-					if (arr[xq][yq] == 1 & !visit[xq][yq]) {
-						queue.add(new int[] { xq, yq });
-						visit[xq][yq] = true;
-					}
+				int xq = dot.x + dx[i];
+				int yq = dot.y + dy[i];
+
+				if (xq < 0 || yq < 0 || xq >= N || yq >= M||arr[xq][yq]!=0) {
+					continue;
 				}
+				arr[xq][yq] = arr[dot.x][dot.y] + 1;
+				q.add(new dot(xq, yq));
 			}
 
 		}
+
+		
 
 	}
 
